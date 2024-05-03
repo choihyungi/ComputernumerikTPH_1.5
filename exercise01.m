196c7df04d42d15ab5dce483b82fcfa7cfc144c4
Hyungi Choi <choihyungi92@gmail.com>
2024-05-03 18:57:31 +0200

    20240503_1857

--- exercise01.m
+++ exercise01.m
@@ -1,3 +1,20 @@
 clear all;
 close all;
 
+%5.1
+%
+
+%5.2
+N = 10;
+xmin = 0;
+xmax = 0.9;
+
+xs = xmin:xmax/N:xmax;
+ys = zeros(length(xs), 1);
+for k = 1:length(xs)
+    x = xs(k);
+    ys(k) = minimum_n(x);
+end
+
+figure
+plot(xs, ys);
--- minimum_n.m
+++ minimum_n.m
@@ -3,16 +3,18 @@ function n = minimum_n(x)
     n = 0;
     rel_err = 1;
     comp = atan(x);
-    %comp = exp(x);
     arctan = 0;
     while rel_err > 100*eps
-
-        arctan = arctan + taylor_arctan(x, n);
+        arctan = arctan + taylorN_arctan(x, n);
         rel_err = abs((arctan-comp) / comp);
-        if mod(n, 10000000) == 0
-            disp(n);
-            disp(rel_err);
-        end
+        %if mod(n, 10000000) == 0
+        %    disp(n);
+        %    disp(rel_err);
+        %end
         n = n + 1;
     end
+    n = n - 1;
+    disp(x);
+    disp(n);
+    disp(rel_err);
 end
