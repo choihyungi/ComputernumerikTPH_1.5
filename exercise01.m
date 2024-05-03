4d67372fc134dd73894a0a610cf87f983ea2c63c
Hyungi Choi <choihyungi92@gmail.com>
2024-05-03 21:58:02 +0200

    20240503_2157: 5.3 not tested yet

--- exercise01.m
+++ exercise01.m
@@ -2,7 +2,6 @@ clear all;
 close all;
 
 %5.1
-%
 
 %5.2
 N = 10;
@@ -13,6 +12,19 @@ xs = xmin:xmax/N:xmax;
 ys = zeros(length(xs), 1);
 for k = 1:length(xs)
     x = xs(k);
+    ys(k) = minimum_n(x);
+end
+
+figure
+plot(xs, ys);
+
+%5.3
+N = 100;
+xs = zeros(N, 1);
+ys = zeros(N, 1);
+for k = 1:N
+    x = 1 - 2^(-k);
+    xs(k) = x;
     ys(k) = minimum_n(x);
 end
 
