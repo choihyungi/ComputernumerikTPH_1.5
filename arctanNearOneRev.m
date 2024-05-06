94445ca88455bac3c8725828ad3891b3040f0ac5
s m <sebastian.matkovich@gmail.com>
2024-05-06 16:39:33 +0200

    arctan rev added

---
+++ arctanNearOneRev.m
@@ -0,0 +1,13 @@
+function arctan = arctanNearOneRev(delta)
+arctan = 0;
+arctan = arctan + delta^11/704;
+arctan = arctan - delta^10/320;
+arctan = arctan + delta^9/288;
+arctan = arctan - delta^7/112;
+arctan = arctan + delta^6/48;
+arctan = arctan - delta^5/40;
+arctan = arctan + delta^3/12;
+arctan = arctan - delta^2/4;
+arctan = arctan + delta/2;
+arctan = arctan + pi/2;
+end
